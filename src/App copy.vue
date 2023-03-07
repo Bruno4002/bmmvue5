66947ade51739516c1ea2e23c806172985381d32
= <=>
2023-03-07 11:51:23 -0300

    tyrt

---
+++ src/App copy.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {ref} from 'vue'
+const contador = ref(10)
+ function incrementar() {
+  contador.value++
+ }
+ function decrementar() {
+  if(contador.value > 0)
+  contador.value--
+ }
+ function resetar() {
+  contador.value = 0
+ }
+
+</script>
+
+<template>
+  <button @click="incrementar">+FLA</button>
+  <button @click="decrementar">MENGO-</button>
+  <button @click="resetar">MAIORC</button>
+  <p>Valor do contador: {{ contador }}</p>
+</template>
+
+<style scoped>
+button {
+  width:200px;
+  height: 200px;
+  background-image:linear-gradient(black,red,black,red,black,red,black);
+  color: black;
+  border-radius: 45%;
+  font-size: 50px;
+  border: none;
+  margin-left: 20px;
+  margin-bottom: 20px;
+}
+
+</style>
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+export default{
+data() {
+  return{
+    contador: 0
+  }
+},
+methods: {
+  Incrementar(){
+    this.contador++
+  } ,
+  decrementar(){
+    if(this.contador > 0){
+    this.contador--
+    }
+  },
+ resetar(){
+  this.contador = 0
+ }
+}
+}
+
+ 
+
+</script>
+
+<template>
+  <button @click="Incrementar">+FLA</button>
+  <button @click="decrementar">MENGO-</button>
+  <button @click="resetar">MAIORC</button>
+  <p>Valor do contador: {{ contador }}</p>
+</template>
+
+<style scoped>
+button {
+  width:200px;
+  height: 200px;
+  background-image:linear-gradient(black,red,black,red,black,red,black);
+  color: black;
+  border-radius: 45%;
+  font-size: 50px;
+  border: none;
+  margin-left: 20px;
+  margin-bottom: 20px;
+}
+
+</style>
